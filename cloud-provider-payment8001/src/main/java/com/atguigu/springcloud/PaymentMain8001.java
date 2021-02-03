301c2a044b82305a6472f1e6da3c310b32b39991
ext.muzhennan <mu_zhennan@126.com>
2021-02-03 11:23:37 +0800

    初始化

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动
+ *
+ * @author ext.muzhennan
+ * @version 1.0
+ * @date 2021/2/3 11:09
+ */
+
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
+}
