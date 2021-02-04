11df1288480a960012b8eba33b9d558d7cdb22f5
zhennan <mu_zhennan@126.com>
2021-02-04 22:17:15 +0800

    临时提交

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/entities/CommonResult.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/entities/CommonResult.java
@@ -5,17 +5,23 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
+//@AllArgsConstructor
 @NoArgsConstructor
 public class CommonResult<T> {
+
+    private Integer code;
+    private String message;
+    private T data;
 
     public CommonResult(Integer code, String message) {
         this(code, message, null);
     }
 
-    private Integer code;
-    private String message;
-    private T data;
+    public CommonResult(Integer code, String message, T data) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
 
 
 }
