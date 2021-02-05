e87a3232888827ed13eb8233908c79f6ce058902
ext.muzhennan <mu_zhennan@126.com>
2021-02-05 18:10:41 +0800

    负责均衡

--- cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/PaymentMain8002.java
+++ cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/PaymentMain8002.java
@@ -2,13 +2,13 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaServer
 @SpringBootApplication
-public class EurekaMain7001 {
+@EnableEurekaClient
+public class PaymentMain8002 {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaMain7001.class,args);
+        SpringApplication.run(PaymentMain8002.class, args);
     }
 }
